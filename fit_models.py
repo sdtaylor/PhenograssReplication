1d034f7eaf254c460f2c3e9379c74ade36c45a57
Shawn <shawntaylor@weecology.org>
2020-03-10 12:28:43 -0600

    updates to model fitting
    - include model sets in the fitting procedure
    - move some important variables to the top for easy adjustments
    - calculate workers automatically

--- fit_models.py
+++ fit_models.py
@@ -14,7 +14,38 @@ from dask.distributed import Client, as_completed
 from dask import delayed
 import dask
 
-ceres_workers = 140 # the number of slurm job that will be spun up
+###############################################
+
+#models_to_fit = ['Naive','CholerPR1','CholerPR2','CholerPR3','PhenoGrassNDVI']
+models_to_fit = ['Naive','NaiveMAPCorrected']
+
+model_fitting_note = 'test run'
+
+fitting_sets = pd.read_csv('model_fitting_set_info/fitting_sets.csv')
+fitting_set_assignments = pd.read_csv('model_fitting_set_info/fitting_set_assignments.csv')
+
+###############################################3
+# Dask/ceres stuff
+chunks_per_job = 20
+parameters_count = 8 # phenograss parameters since its the most intensive. 
+de_popsize     = 4
+de_maxiter     = 2
+
+use_default_workers = False
+default_workers = 100
+
+# While fitting scipy.differential_evolution will generate popsize*params
+# jobs. These are chunked together by dask_scipy_mapper to be more efficient.
+# the best number of workers is the amount that can run all those in one go. 
+# Note the number of workers is the number of slurm jobs that will be spun up
+if use_default_workers:
+    print('Using default number of workers: ', str(default_workers))
+    ceres_workers = default_workers
+else:
+    ceres_workers = int(np.ceil(de_popsize * parameters_count / chunks_per_job))
+    eq = '{pop}*{param}/{c} = {w}'.format(pop=de_popsize, param=parameters_count, c=chunks_per_job, w=ceres_workers)
+    print('Calculating optimal number of workers: ' + eq)
+
 ceres_cores_per_worker = 1 # number of cores per job
 ceres_mem_per_worker   = '2GB' # memory for each job
 ceres_worker_walltime  = '48:00:00' # the walltime for each worker, HH:MM:SS
@@ -29,9 +60,7 @@ ceres_partition        = 'short'    # short: 48 hours, 55 nodes
 phenocam_info = pd.read_csv('site_list.csv')
 phenocam_info = phenocam_info[phenocam_info.has_processed_data]
 
-# training_pixels = pixel_info[pixel_info.is_training].pixel_id.values
 
-# training_year_sets = [range(year_set['start'],year_set['end']+1) for year_set in config['training_year_sets']]
 ######################################################
 # Setup dask cluster
 ######################################################
@@ -51,9 +80,10 @@ while active_workers < (ceres_workers-1):
 print('all workers online')
 #dask_client = Client()
 
+
 # This is the callable sent to scipy.
 def dask_scipy_mapper(func, iterable, c=dask_client):
-    chunked_iterable = toolz.partition_all(10, iterable)
+    chunked_iterable = toolz.partition_all(chunks_per_job, iterable)
     results = [func(x) for x in chunked_iterable]
     futures =  dask_client.compute(results)
     return list(toolz.concat([f.result() for f in futures]))
@@ -64,10 +94,8 @@ def dask_scipy_mapper(func, iterable, c=dask_client):
 loss_function = 'mean_cvmae'
 
 de_fitting_params = {
-                     #'maxiter':1000,
-                     #'popsize':200,
-                     'maxiter':10,
-                     'popsize':20,
+                     'maxiter':de_maxiter,
+                     'popsize':de_popsize,
                      'mutation':(0.5,1),
                      'recombination':0.25,
                      'workers': dask_scipy_mapper,
@@ -111,11 +139,15 @@ parameter_ranges = {'CholerPR1':{'b1':(0,200),
                                   'Phmax': (1,50),
                                   'Phmin': (1,50),
                                   'Topt': (0,45), 
-                                  'h': 222, # TODO: let this vary when fitting > 1 site
+                                  'h': (1,1000), # TODO: let this vary when fitting > 1 site
                                   #'h': (1,1000),
                                   'L': (0,30)},
                     'Naive' : {'b1':(0,100),
                                'b2':(0,100),
+                               'L': (0,60)},
+                    'NaiveMAPCorrected' : {'b1':(0,100),
+                               'b2':(0,100),
+                               'h':(1,1000),
                                'L': (0,60)}
                     }
 
@@ -133,14 +165,18 @@ def load_model_and_data(model_name,timeseries_ids, years):
 if __name__=='__main__':
     fit_models = []
     
-    for timeseries_set, timeseries_id in enumerate(phenocam_info.timeseries_id):
-        #for model_name in ['Naive','CholerPR1','CholerPR2','CholerPR3','PhenoGrassNDVI']:
-        for model_name in ['Naive']:
-            
+    total_sets = len(fitting_sets)
+    
+    for fitting_set_i, this_fitting_set in enumerate(fitting_sets.model_fitting_sets):
+        this_set_timeseries = fitting_set_assignments[fitting_set_assignments.model_fitting_sets==this_fitting_set].timeseries_id
+        
+        for model_name in models_to_fit:
+            print('scheduler address: '+dask_client.scheduler_info()['address'])
+            print('set {s} - {n}/{t} - {m}'.format(s=this_fitting_set,n=fitting_set_i, t=total_sets,m=model_name))
             # This future is the model, with fitting data, being loaded on all
             # the nodes by replicate()
             model_future = dask_client.submit(load_model_and_data, model_name = model_name, 
-                                              timeseries_ids = [timeseries_id], years = 'all')
+                                              timeseries_ids = this_set_timeseries, years = 'all')
             dask_client.replicate(model_future)
             
             # Keep a local model for some scipy fitting stuff
@@ -168,12 +204,12 @@ if __name__=='__main__':
                             'loss_function'    : loss_function,
                             'optimize_output'  : dict(scipy_output)}
             local_model.update_metadata(fitting_info = fitting_info)
-            local_model.update_metadata(timeseries_set =  timeseries_set)
-            local_model.update_metadata(timeseries_used = timeseries_id)
+            local_model.update_metadata(fitting_set =  this_fitting_set)
+            local_model.update_metadata(timeseries_used = ','.join([str(t) for t in this_set_timeseries]))
 
             fit_models.append(local_model)
     
     
     # compile all the models into a set and save
-    model_set = load_models.make_model_set(fit_models, note='test run')
+    model_set = load_models.make_model_set(fit_models,  note=model_fitting_note)
     load_models.save_model_set(model_set)
