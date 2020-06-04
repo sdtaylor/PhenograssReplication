a94d893d560ecf6f7cf56b6b5a43c9045496a8f2
Shawn <shawntaylor@weecology.org>
2020-06-04 16:16:56 -0600

    fix cols, start on site table

--- make_error_tables.R
+++ make_error_tables.R
@@ -32,11 +32,24 @@ error_table = primary_errors %>%
   left_join(error_table_order, by=c('prediction_set'='model_fitting_sets')) %>%
   arrange(row_order) %>%
   mutate(row_label = cell_spec(row_label, 'latex', bold=(row_order %in% cell_type_1))) %>%
-  select(row_label, allsites, ecoregion, `ecoregion-vegtype`, vegtype)
+  select(row_label, allsites, ecoregion, vegtype,  `ecoregion-vegtype`)
   
-
-table_column_names = c('','All Site Model','Ecoregion Level Model','Vegetation Model','Ecoregion+Vegetation Model')
+# TODO: need cv errors in here for the 2 grassland models
+table_column_names = c('','All Site Model','Ecoregion Model','Vegetation Model','Ecoregion+Vegetation Model')
 
 kable(error_table, 'latex', col.names = table_column_names, escape = F) %>%
   add_indent(c(3,4,6,7,9,10,12))
 
+
+##################################################################################################
+# Supplemental table for site level errors
+
+site_errors = read_csv('results/site_level_error_table.csv')
+site_table_column_names = c('Ecoregion','Veg Type','Site','All Site Model','Ecoregion Level Model','Ecoregion+Vegetation Model','Vegetation Model')
+
+site_error_table = site_errors %>%
+  select(ecoregion_desc, veg_type = roi_type, phenocam_name, allsites, ecoregion, `ecoregion-vegtype`, vegtype) %>%
+  sample_n(20)
+
+kable(site_error_table, 'latex', col.names = site_table_column_names, escape = F) %>%
+  column_spec(1, width = '40em')
