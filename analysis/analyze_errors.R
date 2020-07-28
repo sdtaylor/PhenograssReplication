75858c9bddbd6245f3f4036ebf585d50d07ecd2e
Shawn <shawntaylor@weecology.org>
2020-07-28 13:33:57 -0600

    simplify error tables

--- analysis/analyze_errors.R
+++ analysis/analyze_errors.R
@@ -11,8 +11,8 @@ all_predictions = all_predictions %>%
 
 site_errors = all_predictions %>%
   group_by(fitting_set, model, timeseries_id) %>%
-  summarise(daily_rmse = sqrt(mean( (fCover_observed - fCover_predicted)^2 ,na.rm=T)),
-            daily_r2   = 1  - sum((fCover_observed - fCover_predicted)^2,na.rm=T) / sum((fCover_observed - mean(fCover_observed,na.rm=T))^2,na.rm=T),
+  summarise(rmse = sqrt(mean( (fCover_observed - fCover_predicted)^2 ,na.rm=T)),
+            r2   = 1  - sum((fCover_observed - fCover_predicted)^2,na.rm=T) / sum((fCover_observed - mean(fCover_observed,na.rm=T))^2,na.rm=T),
             n=n(),
             percent_na=mean(is.na(fCover_observed))) %>%
   ungroup() 
@@ -20,8 +20,8 @@ site_errors = all_predictions %>%
 primary_errors = site_errors %>%
   filter(model == 'PhenoGrass') %>% # just the phenograss model in the primary things. naive errors will be in supplement
   group_by(fitting_set, model) %>%
-  summarise(daily_rmse = round(mean(daily_rmse),2),
-            daily_r2 =   round(mean(daily_r2),2),
+  summarise(rmse = round(mean(rmse),2),
+            r2 =   round(mean(r2),2),
             n_timeseries = n_distinct(timeseries_id)) %>%
   ungroup()
 
