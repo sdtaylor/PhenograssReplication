a1ef0308c7b243e165aff2dff7933b01d99d79a4
Shawn <shawntaylor@weecology.org>
2020-12-27 12:12:30 -0700

    Change error metrics in scripts, figures, & tables
    R2 is now called the nash sutcliffe coeficient of efficiency, NSE,
    note the math is the same
    
    RMSE is removed since it isn't comparable across sites. replaced
    by the mean coefficient of variation of the mean absolute error,
    aka the mean cvmae, aka F. This is also the loss metric used in
    fitting.

--- analysis/analyze_cv_errors.R
+++ analysis/analyze_cv_errors.R
@@ -11,9 +11,8 @@ cv_predictions = read_csv('data/cv_model_predictions.csv',
 
 site_level_errors = cv_predictions %>%
   group_by(fitting_set, model, left_out_timeseries) %>%
-  summarise(rmse = sqrt(mean( (gcc_observed - gcc_predicted)^2 ,na.rm=T)),
-            r2   = 1  - sum((gcc_observed - gcc_predicted)^2,na.rm=T) / sum((gcc_observed - mean(gcc_observed,na.rm=T))^2,na.rm=T),
-            #mae  = mean(abs(gcc_observed - gcc_predicted),na.rm=T),
+  summarise(cvmae = mean(abs(gcc_observed - gcc_predicted), na.rm=T)/mean(gcc_observed, na.rm=T) ,
+            nse   = 1  - sum((gcc_observed - gcc_predicted)^2,na.rm=T) / sum((gcc_observed - mean(gcc_observed,na.rm=T))^2,na.rm=T),
             n=n(),
             n_timeseries = n_distinct(timeseries_id),
             percent_na=mean(is.na(gcc_observed))) %>%
@@ -24,7 +23,8 @@ assertthat::assert_that(all(site_level_errors$n_timeseries==1), msg='>1 timeseri
 
 site_level_errors %>%
   group_by(fitting_set, model) %>%
-  summarise(avg_r2 = mean(r2),
-            avg_rmse = mean(rmse)) %>%
+  summarise(avg_nse = mean(nse),
+            mean_cvmae = mean(cvmae),
+            n_sites = n()) %>%
   ungroup()
   
--- analysis/make_error_tables.R
+++ analysis/make_error_tables.R
@@ -35,12 +35,12 @@ error_table = primary_errors %>%
   full_join(error_table_order, by=c('fitting_set'='model_fitting_sets')) %>%
   arrange(row_order) %>%
   mutate(row_label = cell_spec(row_label, 'latex', bold=(row_order %in% bold_row_titles))) %>%
-  mutate(r2 = cell_spec(r2, 'latex', bold=(r2>=0.65))) %>%
-  mutate(r2 = ifelse(r2=='NA',NA,r2)) %>% # cell_spec is inserting literal NA here
-  select(row_label, r2, rmse, n_timeseries, site_years)
+  mutate(nse = cell_spec(nse, 'latex', bold=(nse>=0.65))) %>%
+  mutate(nse = ifelse(nse=='NA',NA,nse)) %>% # cell_spec is inserting literal NA here
+  select(row_label, nse, mean_cvmae, n_timeseries, site_years)
   
 # TODO: need cv errors in here for the 2 grassland models
-table_column_names = c('','R\textsuperscript{2}','RMSE','Num. Sites','Site Years')
+table_column_names = c('','NSE','F','Num. Sites','Site Years')
 
 kable(error_table, 'latex', col.names = table_column_names, escape = F) %>%
   add_indent(c(6,7,9,10,12,13,15))
@@ -59,15 +59,15 @@ site_info = read_csv('site_list.csv') %>%
    left_join(ecoregion_info, by='ecoregion') %>%
    select(timeseries_id, site_label, ecoregion_abbr)
 
-# Arrange the csv file from columns c('fitting_set','model','timeseries_id','rmse','r2') 
-# to c('site_label','fitting_set','model1_r2','model1_rmse','model2_r2',...)
+# Arrange the csv file from columns c('fitting_set','model','timeseries_id','cvmae','nse') 
+# to c('site_label','fitting_set','model1_nse','model1_cvmae','model2_nse',...)
 
 site_errors = read_csv('results/site_level_error_table.csv') %>% 
    filter(timeseries_id != 32) %>% # drop jasperridge roi2000 here while the new models run.
    mutate(model = recode(model, 'NaiveMAPCorrected' = 'Naive')) %>%
    left_join(site_info, by='timeseries_id') %>%
-   select(fitting_set, model, rmse, r2, site_label, ecoregion_abbr) %>%
-   gather(error_type, error_value, r2, rmse) %>%
+   select(fitting_set, model, cvmae, nse, site_label, ecoregion_abbr) %>%
+   gather(error_type, error_value, nse, cvmae) %>%
    mutate(error_value = round(error_value,2)) %>%
    unite(error_label, model, error_type) %>%
    spread(error_label, error_value)
@@ -92,7 +92,7 @@ fitting_set_labels = tribble(
 
 site_error_table = site_errors %>%
    left_join(fitting_set_labels, by='fitting_set') %>%
-   select(ecoregion_abbr, site_label, fitting_label, PhenoGrass_r2, PhenoGrass_rmse, Naive_r2, Naive_rmse) %>%
+   select(ecoregion_abbr, site_label, fitting_label, PhenoGrass_nse, PhenoGrass_cvmae, Naive_nse, Naive_cvmae) %>%
    arrange(ecoregion_abbr, site_label) 
 
 # put in blanks so each ecoregion and site are only listed on their first respective row\
